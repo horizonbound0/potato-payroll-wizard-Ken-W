9358ef009a5827c53fa0cd12153bedd6210cedc6
Kenneth A Wagner <kenw2190@gmail.com>
2024-06-03 16:35:01 -0500

    Added comments on my logic. Looking forward to comments on best practices or tip/tricks!

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,8 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 const employeesArray = [
+
+  // adding some objects outside of the collect employees function to make sure it's constructing correctly outside of the function.
   {
     firstName: "Alexander",
     lastName: "The Average",
@@ -18,17 +20,21 @@ const employeesArray = [
 const collectEmployees = function () {
   // TODO: Get user input to create and return an array of employee objects
 
+  // Start the loop of window API prompts for filling the employeesArray
   while (window.confirm("Do you want to add an employee?")) {
 
+    // creating the object to be filled later
     let employee = {
       firstName: "",
       lastName: "",
       salary: 0
     };
 
+    // have the user input the first and last name
     employee.firstName = window.prompt(`First name: `, `First name...`);
     employee.lastName = window.prompt(`Last name: `, `Last name...`);
 
+    // make a function to make sure the salary number is in fact a number.
     function salaryNumber(x) {
       if (isNaN(x)) {
         return 0;
@@ -36,16 +42,15 @@ const collectEmployees = function () {
         return x;
       }
     }
+
+    // grab the salary number from the user, while simultaneously checking to make sure it is actually a number. If it isn't, default the salary to zero.
     const salaryNaN = salaryNumber(window.prompt(`Salary: `, `Amount...`));
 
     employee.salary = parseInt(salaryNaN);
 
-    //salaryNumber(employee.salary);
-
+    // push the newly created object into the array.
     employeesArray.push(employee);
   }
-
-  console.log(`---`);
   
   return employeesArray;
 }
@@ -54,14 +59,18 @@ const collectEmployees = function () {
 const displayAverageSalary = function () {
   // TODO: Calculate and display the average salary
 
+  // declare/define a variable to hold the total of all salary properties.
   let totalSalary = 0;
 
+  // run through the employeesArray and grab each objects salary
   for (let employee of employeesArray) {
     const employeeSalary = employee.salary;
     
+    // calculating.... calculating....
     totalSalary = (totalSalary + employeeSalary);
   }
 
+  // using math.floor to round down the average to a whole number.
   const salaryAverage = Math.floor(totalSalary / employeesArray.length);
 
   console.log(`The average salary from our ${employeesArray.length} employees is $${salaryAverage}.00`);
@@ -72,8 +81,11 @@ const displayAverageSalary = function () {
 // Select a random employee
 const getRandomEmployee = function () {
   // TODO: Select and display a random employee
+
+  // declare/define the variable that we'll use for passing into the employeesArray call
   let empIndex = 0;
 
+  // making a function for randomizing the empIndex
   const setEmpIndex = function() {
     empIndex =  Math.floor(Math.random() * employeesArray.length);
     return empIndex;
@@ -81,6 +93,7 @@ const getRandomEmployee = function () {
 
   setEmpIndex();
   
+  // pass the randomized employee into the console. Congrats!
   console.log(`Huzzah to ${employeesArray[empIndex].firstName} ${employeesArray[empIndex].lastName} for winning our random drawing!`);
   
   return empIndex;
